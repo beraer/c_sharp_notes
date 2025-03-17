682d1c29028d547f9f8e6b9e0a8d6bf8d72c8e3d
beraer <s29315@pjwstk.edu.pl>
2025-03-17 14:46:35 +0100

    Collections module was finished and tested as well, assignement was finished.

--- Tutorial2_parent/Tutorial2/Collections/Collections.cs
+++ Tutorial2_parent/Tutorial2/Collections/Collections.cs
@@ -4,5 +4,33 @@ namespace Tutorial2.Collections;
 
 public class Collections
 {
-    // Write your code here
+   public static List<StudentResult> ProcessGrades(List<Student> students)
+   {
+      var results = new List<StudentResult>();
+      foreach (var stud in students)
+      {
+         string result = "";
+         if (stud.Grade >= 90)
+         {
+            result = "Exemption";
+         }
+
+         if (stud.Grade >= 50 && stud.Grade < 90)
+         {
+            result = "Passing";
+         }
+
+         if (stud.Grade < 50)
+         {
+            result = "Needs Improvement";
+         }
+         
+         results.Add(new StudentResult()
+         {
+            Name = stud.Name,
+            Result = result
+         });
+      }
+      return results;
+   }
 }
